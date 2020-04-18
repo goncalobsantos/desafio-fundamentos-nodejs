d13831d3507d49393af332f20147af58da775fa6
Gonçalo Santos <goncalobisantos@gmail.com>
2020-04-18 21:44:46 +0100

    All TODO are done. Tests passed Why: * Code was still incomplete This change addresses the need by: * The completion of code neeeded to pass the tests Notes: * Part of challenges of GoStack bootcamp

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -6,6 +6,12 @@ interface Balance {
   total: number;
 }
 
+interface CreateTransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class TransactionsRepository {
   private transactions: Transaction[];
 
@@ -14,15 +20,38 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const incomes = this.transactions.filter(
+      transaction => transaction.type === 'income',
+    );
+    const outcomes = this.transactions.filter(
+      transaction => transaction.type === 'outcome',
+    );
+    const income = incomes.reduce(
+      (acc, transaction) => acc + transaction.value,
+      0,
+    );
+    const outcome = outcomes.reduce(
+      (acc, transaction) => acc + transaction.value,
+      0,
+    );
+
+    const balance = {
+      income,
+      outcome,
+      total: income - outcome,
+    };
+
+    return balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, type, value }: CreateTransactionDTO): Transaction {
+    const newTransaction = new Transaction({ title, type, value });
+    this.transactions.push(newTransaction);
+    return newTransaction;
   }
 }
 
--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,6 +1,11 @@
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
 
+interface RequestDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
 class CreateTransactionService {
   private transactionsRepository: TransactionsRepository;
 
@@ -8,8 +13,21 @@ class CreateTransactionService {
     this.transactionsRepository = transactionsRepository;
   }
 
-  public execute(): Transaction {
-    // TODO
+  public execute({ title, value, type }: RequestDTO): Transaction {
+    const balance = this.transactionsRepository.getBalance();
+    const validInsertion =
+      type === 'outcome' ? balance.total - value > 0 : true;
+    if (!validInsertion) {
+      throw Error(
+        `You have insufficient funds to make that transaction. Current balance is: ${balance.total}`,
+      );
+    }
+    const transaction = this.transactionsRepository.create({
+      title,
+      value,
+      type,
+    });
+    return transaction;
   }
 }
 
